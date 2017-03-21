f2dc4fa86a5e668e1fe94b252f8098f4edb55f2d
Marina <tarakanovandy@gmail.com>
2017-03-21 14:16:36 +0100

    Heroku deploy

--- src/main/java/com/inspoweb/utils/schedule/ReminderTimerTask.java
+++ src/main/java/com/inspoweb/utils/schedule/ReminderTimerTask.java
@@ -40,6 +40,7 @@ public class ReminderTimerTask extends TimerTask {
         try {
             BufferedImage img = ImageIO.read(new URL(reminder.getImageLink()));
             ImageIcon icon = new ImageIcon(Thumbnails.of(img).forceSize(300, 300).asBufferedImage());
+            System.setProperty("DISPLAY","0:0");
             jOptionPane.showMessageDialog(null, reminder.getText(), "Reminder", JOptionPane.DEFAULT_OPTION, icon);
         } catch (IOException e) {
             e.printStackTrace();
