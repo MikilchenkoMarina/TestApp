131fcefa33fa14e6763c7d5bb24a3700bf502a2a
Marina <tarakanovandy@gmail.com>
2017-04-04 16:18:40 +0200

    js popaps added

---
+++ Readme.md
@@ -0,0 +1,23 @@
+<h3>
+Inspominder - Spring application 
+</h3>
+<h4>
+<bold>Summary:</bold>
+Thi is an application for memorizing important information, studying .
+<p>
+The user must register.  After that, he can create any reminders for himself, in his reminder list.
+ </p>
+ <p>
+On the user's profile page, the user can run reminders. After that they will appear in the form of pop-up windows with selected frequency.</p>
+</h4>
+
+<ul>
+<li> Spring RESTfull service(@RestController is used) </li>
+<li> Angular client part</li>
+<li> PostgreSQL database</li>
+<li> Spring JPA (repository extends JpaRepository)</li>
+<li> Bootstrap css is used</li>
+<li> One Jsp view is used - for single-page implementation .  </li>
+</ul>
+
+<h2> see application here https://inspominder.herokuapp.com// </h2>
--- src/main/java/com/inspoweb/configs/WebConfig.java
+++ src/main/java/com/inspoweb/configs/WebConfig.java
@@ -6,16 +6,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
-import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
-import org.springframework.web.servlet.handler.AbstractHandlerMapping;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
@@ -29,15 +25,6 @@ import java.util.Locale;
 @ComponentScan(basePackages = "com.inspoweb")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-
-/*    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        resolver.setExposeContextBeansAsAttributes(true);
-        return resolver;
-    }*/
 
     @Bean
     public MessageSource messageSource() {
